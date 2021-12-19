24b1e4f9230613d051ee1db4e412b9e5e786aaac
enesusta <enesusta@email.com>
2021-12-20 00:01:35 +0300

    "Update"

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+
+# delete existing hamal container
+docker rm -f $(docker container ls | grep hamal | cut -d' ' -f1)
+
+docker build -t hamal .
 docker run -d -e RULES_FILE=redirections.yml -p 80:80 -v $(pwd)/redirections.yml:/redirections.yml:rw hamal
 #docker run -e RULES_FILE=redirections.yml -p 80:80 -v $(pwd)/redirections.yml:/redirections.yml hamal
